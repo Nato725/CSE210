38687bd4bc47146a2185e666b9826c5952ec2eee
Nato725 <hut22006@byui.edu>
2026-07-11 22:24:57 -0600

    Create product.cs

---
+++ final/Foundation2/product.cs
@@ -0,0 +1,35 @@
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private double _price;
+    private int _quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public decimal GetPrice()
+    {
+        return _price;
+    }
+
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+
+    public decimal GetTotalValue()
+    {
+        return _price * _quantity;
+    }
+}
