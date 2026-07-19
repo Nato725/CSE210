1447171f2e8293cd9f0ef73cfea6330b02ba1897
Nato725 <hut22006@byui.edu>
2026-07-18 21:44:22 -0600

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Running runActivity = new Running("01 Aug 2026", 10, 1.0);
+        Cycling bikeActivity = new Cycling("02 Aug 2026", 15, 15.0);
+        Swimming swimActivity = new Swimming("03 Aug 2026", 10, 5);
+
+        List<Activity> activitiesList = new List<Activity>
+        {
+            runActivity,
+            bikeActivity,
+            swimActivity,
+        };
+
+    Console.WriteLine("EOS Fitness Activity Log:");
+
+    foreach (Activity activity in activitiesList)
+    {
+        Console.WriteLine(activity.GetSummary());
+    }
     }
 }
