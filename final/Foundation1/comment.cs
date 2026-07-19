bc2e5a2c87f53e9cb658e3b7783b57de7f9ed187
Nato725 <hut22006@byui.edu>
2026-07-18 18:06:03 -0600

    Update comment.cs

--- final/Foundation1/comment.cs
+++ final/Foundation1/comment.cs
@@ -1,11 +1,11 @@
 public class Comment
 {
-    public string CommenterName { get; set; }
-    public string CommentText { get; set; }
-
+    private string _commenterName;
+    private string _commentText;
+    
     public Comment(string commenterName, string commentText)
     {
-        CommenterName = commenterName;
-        CommentText = commentText;
+        _commenterName = commenterName;
+        _commentText = commentText;
     }
 }
